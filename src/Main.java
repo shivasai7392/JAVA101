0455a40609868c8aa6ed03db147afb4a4e7eeabb
Shiva <s.krishna@beta-cae.in>
2025-04-16 17:29:08 +0530

    Number printer

--- src/Main.java
+++ src/Main.java
@@ -5,45 +5,50 @@ import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) {
-       List<String> myList = new ArrayList<>();
-       myList.add("Python");
-       myList.add("java");
-       myList.add("C++");
-       System.out.println("List of elements : "+myList);
+//       List<String> myList = new ArrayList<>();
+//       myList.add("Python");
+//       myList.add("java");
+//       myList.add("C++");
+//       System.out.println("List of elements : "+myList);
+//
+//       Set<String> mySet = new HashSet<>();
+//       mySet.add("Apple");
+//       mySet.add("Banana");
+//       System.out.println("Set of elements : "+mySet);
+//
+//       Map<String, Integer> myMap = new HashMap<>();
+//       myMap.put("Apple", 1);
+//       myMap.put("Banana", 2);
+//       System.out.println("Map of elements : "+myMap);
+//
+//       Iterator<String> iterator = myList.iterator();
+//       while (iterator.hasNext()) {
+//          System.out.println(iterator.next());
+//          iterator.remove();
+//       }
+//       System.out.println("List of elements : "+myList);
+//       while (iterator.hasNext()) {
+//          System.out.println(iterator.next());
+//          iterator.remove();
+//       }
+//       PriorityQueue<String> myPriorityQueue = new PriorityQueue<>();
+//       myPriorityQueue.offer("Apple");
+//       myPriorityQueue.offer("Banana");
+//       myPriorityQueue.offer("Python");
+//       myPriorityQueue.offer("C++");
+//
+//       System.out.println("Priority queue : "+myPriorityQueue);
+//
+//       Person person1 = new Person(1, "Shiva", 27);
+//       Person person2 = new Person(2, "Shiva", 27);
+//       Map<Person, Integer> personMap = new HashMap<>();
+//       personMap.put(person1, 1);
+//       personMap.put(person2, 2);
+//       System.out.println("Map of elements : "+personMap);
 
-       Set<String> mySet = new HashSet<>();
-       mySet.add("Apple");
-       mySet.add("Banana");
-       System.out.println("Set of elements : "+mySet);
-
-       Map<String, Integer> myMap = new HashMap<>();
-       myMap.put("Apple", 1);
-       myMap.put("Banana", 2);
-       System.out.println("Map of elements : "+myMap);
-
-       Iterator<String> iterator = myList.iterator();
-       while (iterator.hasNext()) {
-          System.out.println(iterator.next());
-          iterator.remove();
+       for(int i=0; i<100; i++){
+          Thread thread = new Thread(new NumberPrinter(i));
+          thread.start();
        }
-       System.out.println("List of elements : "+myList);
-       while (iterator.hasNext()) {
-          System.out.println(iterator.next());
-          iterator.remove();
-       }
-       PriorityQueue<String> myPriorityQueue = new PriorityQueue<>();
-       myPriorityQueue.offer("Apple");
-       myPriorityQueue.offer("Banana");
-       myPriorityQueue.offer("Python");
-       myPriorityQueue.offer("C++");
-
-       System.out.println("Priority queue : "+myPriorityQueue);
-
-       Person person1 = new Person(1, "Shiva", 27);
-       Person person2 = new Person(2, "Shiva", 27);
-       Map<Person, Integer> personMap = new HashMap<>();
-       personMap.put(person1, 1);
-       personMap.put(person2, 2);
-       System.out.println("Map of elements : "+personMap);
     }
 }
--- src/NumberPrinter.java
+++ src/NumberPrinter.java
@@ -1,2 +1,11 @@
-public class NumberPrinter {
+public class NumberPrinter implements Runnable {
+    int number;
+    public NumberPrinter(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + ": " + number);
+    }
 }
