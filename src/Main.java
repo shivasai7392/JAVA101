4f20b74b51cafcd4e7ee78632c0b7f923e031f51
Shiva <s.krishna@beta-cae.in>
2025-04-16 12:51:58 +0530

    List interface and ArrayList class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+       List<String> myList = new ArrayList<>();
+       myList.add("Python");
+       myList.add("java");
+       myList.add("C++");
+
+       System.out.println("List of elements : "+myList);
     }
 }
