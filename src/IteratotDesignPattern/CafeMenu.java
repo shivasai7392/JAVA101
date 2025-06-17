e6876bfc5cd6b0f3436a06f7ce46d16a4ead5e95
Shiva <s.krishna@beta-cae.in>
2025-06-17 18:42:09 +0530

    new cafe menu

---
+++ src/IteratotDesignPattern/CafeMenu.java
@@ -0,0 +1,27 @@
+package IteratotDesignPattern;
+
+import java.util.Hashtable;
+
+public class CafeMenu {
+    Hashtable<String, MenuItem> menuItems = new Hashtable<String, MenuItem>();
+
+    public CafeMenu() {
+        addItem("Veggie", "Veggie Burger", 1.99, false);
+        addItem("Cheese", "Cheese Burger", 1.99, false);
+        addItem("Sauce", "Sauce Burger", 1.99, false);
+        addItem("Pepsi", "Pepsi Burger", 1.99, false);
+        addItem("Coffee", "Coffee Burger", 1.99, false);
+        addItem("Milk", "Milk Burger", 1.99, false);
+        addItem("Chicken", "Chicken Burger", 1.99, false);
+        addItem("Pepsi", "Pepsi Burger", 1.99, false);
+    }
+
+    public void addItem(String name, String description, double price, boolean vegetarian) {
+        MenuItem menuItem = new MenuItem(name, description, price, vegetarian);
+        menuItems.put(name, menuItem);
+    }
+
+    public Hashtable<String, MenuItem> getMenuItems() {
+        return menuItems;
+    }
+}
